7018b7a18e3a00ee140f63df233b259a24175622
Naman Chandak <naman.chandak@hotmail.com>
2024-07-29 12:30:03 +0530

    get by hour

--- src/main/java/com/vertx/Main.java
+++ src/main/java/com/vertx/Main.java
@@ -28,7 +28,8 @@ public class Main {
         public void start(Promise<Void> startPromise) {
             Router router = Router.router(vertx);
 
-            router.get("/").handler(this::handleGetResource);
+            router.get("/").handler(this::handleGetSummaryResource);
+            router.get("/hour").handler(this::handleGetHourResource);
 
             // Initialize AsyncHttpClient
             AsyncHttpClientConfig config = Dsl.config().build();
@@ -47,11 +48,11 @@ public class Main {
                     });
         }
 
-        private void handleGetResource(RoutingContext routingContext) {
+        private void handleGetSummaryResource(RoutingContext routingContext) {
             Vertx vertx = routingContext.vertx();
             vertx.<String>executeBlocking(promise -> {
                 try {
-                    String response = fetchApiData().join(); // Use CompletableFuture's join
+                    String response = fetchSummaryApiData().join(); // Use CompletableFuture's join
                     promise.complete(response);
                 } catch (Exception e) {
                     promise.fail(e);
@@ -65,7 +66,25 @@ public class Main {
             });
         }
 
-        private CompletableFuture<String> fetchApiData() {
+        private void handleGetHourResource(RoutingContext routingContext) {
+            Vertx vertx = routingContext.vertx();
+            vertx.<String>executeBlocking(promise -> {
+                try {
+                    String response = fetchHourApiData().join(); // Use CompletableFuture's join
+                    promise.complete(response);
+                } catch (Exception e) {
+                    promise.fail(e);
+                }
+            }, res -> {
+                if (res.succeeded()) {
+                    routingContext.response().setStatusCode(200).end(res.result());
+                } else {
+                    routingContext.response().setStatusCode(500).end("Failed to fetch data");
+                }
+            });
+        }
+
+        private CompletableFuture<String> fetchSummaryApiData() {
             CompletableFuture<String> future = new CompletableFuture<>();
 
             asyncHttpClient.prepareGet("https://forecast9.p.rapidapi.com/rapidapi/forecast/Berlin/summary/")
@@ -77,8 +96,28 @@ public class Main {
                         if (response.getStatusCode() == 200) {
                             future.complete(response.getResponseBody());
                         } else {
-                            future.completeExceptionally(new RuntimeException("Failed to fetch data"));
+                            future.completeExceptionally(new RuntimeException("Failed to fetch summary data"));
                         }
+                    }).exceptionally(ex -> {
+                        future.completeExceptionally(ex);
+                        return null;
+                    });
+
+            return future;
+        }
+
+        private CompletableFuture<String> fetchHourApiData() {
+            CompletableFuture<String> future = new CompletableFuture<>();
+
+            asyncHttpClient.prepareGet("https://forecast9.p.rapidapi.com/rapidapi/forecast/Berlin/hourly/")
+                    .setHeader("x-rapidapi-key", "14afbecc82mshc0bf7999f7591d4p1e2789jsna173467eac5b")
+                    .setHeader("x-rapidapi-host", "forecast9.p.rapidapi.com")
+                    .execute()
+                    .toCompletableFuture()
+                    .thenAccept(response -> {
+
+                            future.complete(response.getResponseBody());
+
                     }).exceptionally(ex -> {
                         future.completeExceptionally(ex);
                         return null;
